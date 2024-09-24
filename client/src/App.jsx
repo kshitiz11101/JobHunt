ab057dd891ca8e84c814126e06e989868b03ef4e
Kshitiz Sharma <kshitiz01sharma@gmail.com>
2024-09-24 10:40:53 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <h1 className="bg-red-500 text-underline font-bold">Job Hunt</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
