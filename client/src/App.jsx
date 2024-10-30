5e1c23db4a742fc16e9aac22338a98169f12eed9
Kshitiz Sharma <kshitiz01sharma@gmail.com>
2024-10-30 15:49:23 +0530

    Implemented applicants page for job applications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,37 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import {Routes } from 'react-router-dom';
+import FindJobs from "./pages/FindJobs";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SavedJobs from "./components/SavedJobs";
+import JobDetails from "./components/JobDetails";
+import PostJob from "./pages/PostJobPage";
+import ApplicationForm from "./components/ApplicationForm";
+import PostedJobsPage from "./pages/PostedJobsPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import ApplicantsPage from "./pages/ApplicantsPage";
 
 function App() {
  
   return (
     <>
     <BrowserRouter>
-    {/* <Routes>
-      <Route path="/" element={<Home/>} />
-    </Routes> */}
-    <Home/>
-  
+    <Routes>
+    <Route path="*" element={<Home/>}/>
+    <Route path="/user-profile" element={<UserProfilePage/>}/>
+    <Route path="/find-jobs" element={<FindJobs/>}/>
+    <Route path="job-detail" element={<JobDetails/>}/>
+    <Route path="/saved-jobs" element={<SavedJobs/>}/>
+    <Route path="/post-jobs" element={<PostJob/>}/>
+    <Route path="/login" element={<LoginPage/>}/>
+    <Route path="/apply-jobs/:jobId" element={<ApplicationForm/>}/>
+    <Route path="/signup" element={<SignUpPage/>} />
+    <Route path="/posted-jobs/:userId" element={<PostedJobsPage/>}/>
+    <Route path="applicants/:jobId" element={<ApplicantsPage/>}/>
+    </Routes>
     </BrowserRouter>
+
     </>
   )
 }
